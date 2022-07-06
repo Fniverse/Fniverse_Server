fa720688f5aad01038d40c6a8ea3654ef6157e3a
player31-kks <keumks789@naver.com>
2022-07-06 23:36:37 +0900

    [test]

---
+++ src/perfume/application/service/perfumeReview.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PerfumeReviewService {
+  constructor() {}
+
+  public async createReview() {}
+  public async updateReview() {}
+  public async deleteReview() {}
+  public async getReviews() {}
+  public async getReview() {}
+}
